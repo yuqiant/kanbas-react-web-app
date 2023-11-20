42083680c28374912ec55e997b1e93849888bab5
yuqiant <122412689+yuqiant@users.noreply.github.com>
2023-11-20 16:36:57 -0500

    env setting

--- .history/src/Kanbas/Courses/Modules/client_20231120163028.js
+++ .history/src/Kanbas/Courses/Modules/client_20231120163028.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 const COURSES_URL = "http://localhost:4000/api/courses";
-// const MODULES_URL = "https://kanbas-node-server-app-t0ma.onrender.com/api/modules";
-const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `${API_BASE}/modules`;
+const MODULES_URL = "https://kanbas-node-server-app-t0ma.onrender.com/";
 export const deleteModule = async (moduleId) => {
     const response = await axios
         .delete(`${MODULES_URL}/${moduleId}`);
